ae14ca46e7edecc9bdf4c962a89337bd570c08f0
cao-shuai <cao-shuai@github.com>
2018-02-23 09:52:17 +0800

    add threadcout

---
+++ threadcount.sh
@@ -0,0 +1,17 @@
+echo "please input your progressname:";
+read progressname;
+echo "progressname is: " $progressname;
+while [[ 1 ]]; do
+	progresscount=$(busybox pidof ${progressname});
+	#echo ${progresscount}
+	counts=0;
+	for progresspid in ${progresscount};
+	do
+		#statements
+		#echo $progresspid;
+		count=$(ps -t -p $progresspid |busybox wc -l);
+		counts=$((counts+count));
+	done
+	echo "current thread count is: "$counts
+	sleep 1
+done
